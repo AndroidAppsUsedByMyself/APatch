df6fb6355d32206dd35d8adedc9ee2edab0915d9
Sacabambaspis <102341238+DataEraserC@users.noreply.github.com>
2024-12-16 20:58:15 +0800

    chore: using kpimg-linux instead

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -157,7 +157,7 @@ fun downloadFile(url: String, destFile: File) {
 
 registerDownloadTask(
     taskName = "downloadKpimg",
-    srcUrl = "https://github.com/bmax121/KernelPatch/releases/download/$kernelPatchVersion/kpimg-android",
+    srcUrl = "https://github.com/bmax121/KernelPatch/releases/download/$kernelPatchVersion/kpimg-linux",
     destPath = "${project.projectDir}/src/main/assets/kpimg",
     project = project
 )
