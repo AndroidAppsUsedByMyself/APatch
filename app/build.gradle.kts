43d460fc0dbc0bb006b39d79f725ecec6ad07733
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-12-24 02:39:51 +0800

    Revert "chore: using kpimg-linux instead"
    This reverts commit 88b88baf2cc57cbd65048110b12b61b3b5a4db0a.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -157,7 +157,7 @@ fun downloadFile(url: String, destFile: File) {
 
 registerDownloadTask(
     taskName = "downloadKpimg",
-    srcUrl = "https://github.com/bmax121/KernelPatch/releases/download/$kernelPatchVersion/kpimg-linux",
+    srcUrl = "https://github.com/bmax121/KernelPatch/releases/download/$kernelPatchVersion/kpimg-android",
     destPath = "${project.projectDir}/src/main/assets/kpimg",
     project = project
 )
